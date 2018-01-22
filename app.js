f9ad2d934a307c9206fefeef515af6f0bf6fac50
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-01-22 14:27:00 +0800

    Removed database related content

--- app.js
+++ app.js
@@ -2,10 +2,6 @@
 require('dotenv').config();
 const NODE_ENV = process.env.NODE_ENV || 'development' 
 
-const knexFile = require('./knexfile')[NODE_ENV]
-const knex = require('knex')(knexFile)
-
-
 // Dependency Injection for Routers and Service
 const ViewRouter = require('./ViewRouter');
 
